55b1c6ddc3f17453d8b87c578047029763f268aa
Sr. Lapa <jesus.lc.1810@gmail.com>
2026-03-06 13:41:12 -0500

    player movement v1

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var MAX_SPEED := 300
+@export var ACCELETARION := 1200.0
+@export var FRICTION := 1000
 
 
 func _physics_process(delta: float) -> void:
 
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_vector("move_left","move_right","move_up", "move_down")
+	if direction != Vector2.ZERO:
+		var tg_v = direction * MAX_SPEED
+		velocity =velocity.move_toward(tg_v, ACCELETARION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_slide()
