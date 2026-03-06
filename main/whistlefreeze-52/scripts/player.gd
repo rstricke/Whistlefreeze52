e03320407d754b38cb43e397f5eeabf109aa2ba5
Sr. Lapa <jesus.lc.1810@gmail.com>
2026-03-06 13:21:24 -0500

    player scene added
    player added
    input map added

---
+++ main/whistlefreeze-52/scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
