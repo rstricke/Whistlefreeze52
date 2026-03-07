737396766e747a94af3d314c1740819d9411787f
Sr. Lapa <jesus.lc.1810@gmail.com>
2026-03-07 14:33:29 -0500

    enemy anim

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -60,9 +60,7 @@ func _process(_delta: float) -> void:
 			var tween = create_tween()
 			tween.tween_property(self, "position", target_pos, 0.5).set_ease(Tween.EASE_IN)
 			tween.tween_callback(anim.stop)
-			#position = target_pos
-			if new_sprite != null:
-				$Sprite2D.texture = new_sprite
+
 			print("Player at position:", position)
 			end_turn()
 	
