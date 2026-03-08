6c50e09955b4f32e665d6d4a157530a49d0ae457
Sr. Lapa <jesus.lc.1810@gmail.com>
2026-03-08 16:48:33 -0500

    update main

--- game/scripts/main_menu.gd
+++ game/scripts/main_menu.gd
@@ -1,8 +1,17 @@
 extends Control
 
-
+@onready var main: Control = $Main
+@onready var tutorial: Control = $Tutorial
+func _ready() -> void:
+	tutorial.visible = false
+	main.visible = true
+	
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	
 	
+
+func _on_next_button_pressed() -> void:
+	tutorial.visible = true
+	main.visible = false
